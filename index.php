c7405852cc5657a51de3665fbdfb94e7dbeecb52
edodima13 <edodima13@libero.it>
2022-04-28 19:31:04 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+class Product{
+
+    public $name;
+    public $image;
+    public $price;
+    public $description;
+
+    public function __construct($name ,$image ,$price , $description){
+        $this-> name = $_name;
+        $this-> image = $_image;
+        $this-> price = $_price;
+        $this-> description = $_description;
+    }
+
+}
